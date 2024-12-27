1fbb0862cc2f086050285767aa7a88ebfe18ec4b
Kirill-max-bit <soloncenkokirill64@gmail.com>
2024-12-27 08:33:05 +0000

    сделал задачу

---
+++ main26.py
@@ -0,0 +1,27 @@
+combinations = [
+    (True, True, True),
+    (True, True, False),
+    (True, False, True),
+    (True, False, False),
+    (False, True, True),
+    (False, True, False),
+    (False, False, True),
+    (False, False, False)
+]
+
+
+expressions = [
+    "(not (X or Y)) and (not X or not Z)",  
+    "not (not X and Y) or (X and not Z)",   
+    "X or not Y and not (X or not Z)"       
+]
+
+
+for X, Y, Z in combinations:
+    print(f"X={X}, Y={Y}, Z={Z}")
+    
+    for expression in expressions:
+        result = eval(expression)
+        print(f"{expression} -> {result}")
+    
+    print()
