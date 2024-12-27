ed86725b6acd5c978404432c52bb29cb2b3fa396
Kirill-max-bit <soloncenkokirill64@gmail.com>
2024-12-27 08:18:17 +0000

    сделал задачу

---
+++ main24.py
@@ -0,0 +1,20 @@
+def evaluate_expression(expression, X, Y, Z):
+    return eval(expression)
+
+
+num = [
+    "not (Y or not X and Z) or Z",   
+    "X and not (not Y or Z) or Y",  
+    "not (X or Y and Z) or not X"   
+]
+
+
+for X in [True, False]:
+    for Y in [True, False]:
+        for Z in [True, False]:
+            print(f"X={X}, Y={Y}, Z={Z}")
+            for expression in num:
+                result = evaluate_expression(expression, X, Y, Z)
+                print(f"{expression} -> {result}")
+                print()
+
